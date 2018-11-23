54f71c75c3e44b3e574859c9637b0db2f034cec4
Xavier Trinidad <xmtrinidad@gmail.com>
2018-11-23 14:20:19 -0700

    Added alt card styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule }    from '@angular/common/http';
 
-import {MatSlideToggleModule, MatToolbarModule, MatCardModule, MatButtonModule} from '@angular/material';
+import {MatSlideToggleModule, MatToolbarModule, MatCardModule, MatButtonModule, MatIconModule} from '@angular/material';
 import { CardsSectionComponent } from './cards-section/cards-section.component';
 import { CardComponent } from './cards-section/card/card.component';
 import { AltCardComponent } from './cards-section/alt-card/alt-card.component';
@@ -24,7 +24,8 @@ import { AltCardComponent } from './cards-section/alt-card/alt-card.component';
     MatSlideToggleModule,
     MatToolbarModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/cards-section/alt-card/alt-card.component.ts
+++ src/app/cards-section/alt-card/alt-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Card } from 'src/app/models/card';
 
 @Component({
   selector: 'app-alt-card',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alt-card.component.css']
 })
 export class AltCardComponent implements OnInit {
+  @Input() card: Card;
 
   constructor() { }
 
